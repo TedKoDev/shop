9bf445b600be49ef5978609e454614fcdaf085a3
TEDKORDEV <ted.kordev@gmail.com>
2024-01-20 19:33:13 +0900

    react shop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Container, Navbar, Nav, Row, Col } from "react-bootstrap";
+import data from "./data/data.js";
+
+import "./App.css";
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">한글 Square</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+      <Container>
+        <Row>
+          {shoes.map((shoe, i) => {
+            return <Card shoe={shoes[i]} i={i} key={i} />;
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+function Card(props) {
+  return (
+    <Col md>
+      <img src={props.shoe.image} width="80%" />
+
+      <h4>{props.shoe.title}</h4>
+      <p>
+        {props.shoe.content} & {props.shoe.price}
+      </p>
+    </Col>
+  );
+}
